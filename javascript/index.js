0d8582e9f6a4de0da3394861b2f6886802172827
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2023-10-22 15:28:17 +0200

    Added the time for the single locations

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "HH:mm:ss[<small>]A[</small>]"
+  );
+
+  let amsterdamElement = document.querySelector("#amsterdam");
+  let amsterdamDateElement = amsterdamElement.querySelector(".date");
+  let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+  let amsterdamTime = moment().tz("Europe/Amsterdam");
+
+  amsterdamDateElement.innerHTML = amsterdamTime.format("MMMM Do YYYY");
+  amsterdamTimeElement.innerHTML = amsterdamTime.format(
+    "HH:mm:ss[<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
